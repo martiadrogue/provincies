f4d744d8e038a4b834fbcb8f31424e9496cfc6aa
marti adrogue <marti.adrogue@gmail.com>
2015-05-22 10:07:31 +0200

    Add Home and Brand pages.

---
+++ src/Com/Martiadrogue/Provincies/Controller/HomeController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Com\Martiadrogue\Provincies\Controller;
+
+use PDOException;
+
+/**
+ *
+ */
+class HomeController extends ProvinciesController
+{
+    public function __construct()
+    {
+        # code...
+    }
+
+    public function executeIndex()
+    {
+        $data = parent::getMenuLinks();
+        try {
+            $pdo = parent::getService('pdo');
+            $data['provincias'] = $pdo->read('provincias', 'id_provincia', 'provincia');
+
+            return parent::render('home/index.html', $data);
+        } catch (PDOException $ex) {
+            if ($ex->getCode() === '42S02') {
+                // crea taula
+            }
+        }
+
+        return parent::render('home/index.html', []);
+    }
+
+    public function executeAbout()
+    {
+        $data = parent::getMenuLinks();
+        $data['title'] = 'About Framework MVC';
+        $data['message'] = 'Hello from '.__METHOD__;
+
+        return parent::render('home/about.html', $data);
+    }
+
+    public function executeContact()
+    {
+        $data = parent::getMenuLinks();
+        $data['title'] = 'Get In Touch!';
+        $data['message'] = 'Hello from '.__METHOD__;
+
+        return parent::render('home/contact.html', $data);
+    }
+}
---
+++ src/Com/Martiadrogue/Provincies/Controller/ProvinciesController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Com\Martiadrogue\Provincies\Controller;
+
+use Com\Martiadrogue\Mpwarfwk\Controller\BaseController;
+use Com\Martiadrogue\Mpwarfwk\Connection\Http\HtmlResponse;
+
+class ProvinciesController extends BaseController
+{
+    private $rootTemplate = '../view/';
+    private $cacheTemplate = '../cache/template';
+
+    public function __construct()
+    {
+        # code...
+    }
+
+    protected function getMenuLinks()
+    {
+        return array('links' => [
+            'root' => 'http://'.$this->getHttpHost(),
+            'about' => 'http://'.$this->getHttpHost().'/about',
+            'contact' => 'http://'.$this->getHttpHost().'/contact',
+            'search' => 'http://'.$this->getHttpHost().'/calculadora-provincia',
+            'provincia' => 'http://'.$this->getHttpHost().'/provincia/',
+        ]);
+    }
+
+    protected function getHttpHost()
+    {
+        $request = parent::getService('request');
+
+        return $request->getHttpHost();
+    }
+
+    protected function render($path, Array $data)
+    {
+        $template = parent::getService('twig');
+        $template->setTemplateHome($this->rootTemplate);
+        $template->setCacheHome($this->cacheTemplate);
+        $template->loadTemplate($path);
+        $canvas = $template->paint($data);
+
+        return new HtmlResponse($canvas, 200);
+    }
+}
